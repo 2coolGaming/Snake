3a9565fca97b4c7d3838e0bd5b16e03d2dfa0c9d
LakkieGaming <lakkiegaming@gmail.com>
2016-06-02 20:37:06 -0400

    Create Position.java

---
+++ com/schiebros/snake/Position.java
@@ -0,0 +1,16 @@
+package com.schiebros.snake;
+
+public class Position {
+
+	public int x;
+	public int y;
+
+	public Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public Position() {
+	}
+
+}
